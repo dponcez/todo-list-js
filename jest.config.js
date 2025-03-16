45a1ceeea91ea3363e2f6973b141a6f4227b1ec8
Damian Ponce <dponcez_git@hotmail.com>
2025-03-15 23:25:23 -0500

    chore: add Jest configuration and setup for testing environment

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: "jsdom", // Configura el entorno de prueba
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"], // Archivos de prueba
+  collectCoverage: true, // Habilita la recolección de cobertura
+  coverageDirectory: "coverage", // Carpeta donde se guardará la cobertura
+  coverageReporters: ["json", "lcov", "text", "clover"], // Formatos de reporte de cobertura
+  moduleFileExtensions: ["js", "jsx", "json", "node"], // Extensiones de archivos soportadas
+  verbose: true,
+};
