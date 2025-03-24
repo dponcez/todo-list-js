2cb2e31b112d5375db8344869e43c30cca71cb69
Damian Ponce <dponcez_git@hotmail.com>
2025-03-23 22:17:42 -0500

    feat: update jest configuration

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,11 @@
 const { TextEncoder, TextDecoder } = require("util");
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+const { JSDOM } = require("jsdom");
+
+const { window } = new JSDOM("<!DOCTYPE html><html><body></body></html>");
+global.window = window;
+global.document = window.document;
+global.HTMLElement = window.HTMLElement;
+global.Node = window.Node;
