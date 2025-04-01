65f127c789c962fb6c2e592487021d772b02577b
Damian Ponce <dponcez_git@hotmail.com>
2025-03-31 23:15:16 -0500

    feat: enhance todo functionality with input validation and alert system

--- modules/add_todo.js
+++ modules/add_todo.js
@@ -12,15 +12,15 @@ export const initTodoList = () => {
     todoList: selector('[data-list]')
   }
 
+  const container = selector('.container');
+
   const { addTodoBtn, todoInput, todoList } = dataRefs;
 
   const addTodo = () => {
-    const inputValue = todoInput.value;
-    let trash_icon = '<i class="icon fa-solid fa-trash"></i>'
-
-    if(inputValue === ''){
-      alert('Task cannot be empty')
-    }else{
+    const inputValue = todoInput.value.trim();
+    let trash_icon = '<i class="icon fa-solid fa-trash"></i>';
+    
+    if(inputValue){
       const htmlRefs = {
         listItem: createElement('div'),
         newItem: createElement('li'),
@@ -28,25 +28,63 @@ export const initTodoList = () => {
       }
   
       const { listItem, newItem, trashBtn } = htmlRefs;
-  
-      newItem.innerText = String(inputValue.toUpperCase());
+
+      newItem.textContent = inputValue.toUpperCase();
   
       listItem.classList.add('list--items');
       newItem.classList.add('item');
       trashBtn.classList.add('btn', 'trash--btn');
       trashBtn.innerHTML = trash_icon;
 
-      handler(trashBtn, 'click', deleteTodoList)
-
+      handler(trashBtn, 'click', deleteTodoList);
       saveLocalStorage(inputValue);
   
-      listItem.appendChild(newItem);
+      listItem.appendChild(newItem)
       listItem.appendChild(trashBtn);
       todoList.appendChild(listItem);
 
       todoInput.value = '';
       todoInput.focus();
+    }else{
+      try{
+        const template = createElement("div");
+
+        template.classList.add("alert");
+        container.appendChild(template);
+
+        const alert = selector(".alert");
+
+        alert.innerHTML = `
+          <p class="alert--msg">!Task cannot be empty¡</p>
+          <button class="btn close--alert">
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+        `;
+
+        const closeAlertBtn = selector(".close--alert");
+
+        handler(closeAlertBtn, "click", () => {
+          todoInput.disabled = false;
+          addTodoBtn.disabled = false;
+
+          addTodoBtn.style.cursor = "pointer";
+          addTodoBtn.style.pointerEvents = "all";
+          addTodoBtn.style.backgroundColor = "var(--add-color)";
+
+          alert.remove();
+          todoInput.focus();
+        });
+
+        todoInput.disabled = true;
+        addTodoBtn.disabled = true;
+
+        addTodoBtn.style.cursor = "default";
+        addTodoBtn.style.pointerEvents = "none";
+        addTodoBtn.style.backgroundColor = "hsl(202, 18%, 47%)";
+      }catch(error){
+        console.log(`Error while handling alert: ${error.message}`)
+      }
     }
   }
-  handler(addTodoBtn, 'click', debounce(addTodo, 300))
+    handler(addTodoBtn, 'click', debounce(addTodo, 300));
 }
