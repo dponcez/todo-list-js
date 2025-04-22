cdef404a15ac4483483fec8992b91789f9c499ed
Damian Ponce <dponcez_git@hotmail.com>
2025-04-22 18:57:09 -0500

    fix: ensure task is checked before allowing deletion in todo list

--- modules/delete_todo.js
+++ modules/delete_todo.js
@@ -6,11 +6,18 @@ export const deleteTodoList = (event) => {
   const todoItem = item.closest('.list--items');
 
   if(event.stopPropagation) event.stopPropagation();
-  if(todoItem) todoItem.remove();
 
   if(item.classList.contains('trash--btn')){
     const todo = item.parentElement;
-    todo.classList.add('fall');
+    const checkbox = todoItem.querySelector('input[type="checkbox"]');
+
+    if(!checkbox.checked){
+      alert('You must check the task before deleting it!');
+      return
+    }else{
+      todo.classList.add('fall');
+      todoItem.remove()
+    }
 
     deleteLocalStorage(todo);
 
