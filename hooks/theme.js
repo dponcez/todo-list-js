265d5b12fa753cb0a8e0b6f470b66cdf8a9c6910
Damian Ponce <dponcez_git@hotmail.com>
2025-05-04 23:18:51 -0500

    feat: enhance dark mode functionality by adding button state toggling

--- hooks/theme.js
+++ hooks/theme.js
@@ -4,15 +4,22 @@ import { handler } from '../_fns/custom_functions.js';
 export const handleDarkMode = (dark, active, state, main) => {
   const htmlRefs = {
     darkModeButton: selector(`${state}`),
-    body: selector(`${main}`)
+    body: selector(`${main}`),
+    submitBtn: selector('[data-submit]'),
+    trashBtn: selector('.trash--btn')
   }
 
-  const { darkModeButton, body } = htmlRefs;
+  const { darkModeButton, body, submitBtn, trashBtn } = htmlRefs;
 
-  handler(darkModeButton, 'click', () => {
+  handler(darkModeButton, 'click', (event) => {
+    const target = event.target;
+
+    target.classList.toggle(`${active}`);
     body.classList.toggle(`${dark}`);
-    darkModeButton.classList.toggle(`${active}`);
 
+    submitBtn.classList.toggle(`${dark}`);
+    trashBtn.classList.toggle(`${dark}`);
+  
     const storage = (key, value) => localStorage.setItem(key, value);
     body.classList.contains('dark--mode') ? 
       storage('theme', 'true') :
@@ -25,9 +32,15 @@ export const handleDarkMode = (dark, active, state, main) => {
     if(localTheme === value){
       darkModeButton.classList.add(`${active}`);
       body.classList.add(`${dark}`);
+
+      submitBtn.classList.add(`${dark}`);
+      trashBtn.classList.add(`${dark}`);
     }else{
       darkModeButton.classList.remove(`${active}`);
-      body.classList.remove(`${dark}`)
+      body.classList.remove(`${dark}`);
+
+      submitBtn.classList.remove(`${dark}`);
+      trashBtn.classList.remove(`${dark}`);
     }
   }
 
