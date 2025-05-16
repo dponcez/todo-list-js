c384688baedc4c61bf9b82d69f05588773e7b2e6
Damian Ponce <dponcez_git@hotmail.com>
2025-05-15 23:52:00 -0500

    refactor: streamline deleteTodoList function for improved alert handling and code clarity

--- modules/delete_todo.js
+++ modules/delete_todo.js
@@ -3,44 +3,47 @@ import { deleteLocalStorage } from "../hooks/delete_local_storage.js";
 import { createElement } from '../_fns/custom_functions.js';
 import { selector } from '../_fns/custom_functions.js';
 
-const container = selector('.container');
-
 export const deleteTodoList = (event) => {
   const item = event.target;
   const todoItem = item.closest('.list--items');
-  console.log(container)
+  const todo = item.parentElement;
 
-  if(!todoItem) return; 
+  // if(!todoItem) return; 
+  if(!item.classList.contains('trash--btn')) return;
   if(event.stopPropagation) event.stopPropagation();
 
-  if(item.classList.contains('trash--btn')){
-    const todo = item.parentElement;
-    const checkbox = todoItem.querySelector('input[type="checkbox"]');
+  const checkbox = todoItem.querySelector('input[type="checkbox"]');
 
-    if(!checkbox.checked){
-      const template = createElement('div');
+  if(!checkbox.checked){
+    if(!document.querySelector('.delete--container')){
+      const htmlRefs = {
+        template: createElement('div'),
+        alertText: createElement('p'),
+        alertBtn: createElement('button')
+      }
+
+      const { template, alertText, alertBtn } = htmlRefs;
+
       template.classList.add('delete--container');
+      alertText.classList.add('alert--text');
+      alertBtn.classList.add('btn', 'alert--btn');
 
-      container.appendChild(template);
-      const alert = selector('.delete--container');
+      alertText.textContent = 'You must check the task before deleting it!';
+      alertBtn.textConten = 'OK';
 
-      alert.innerHTML = `
-        <div class="alert--container">
-          <p class="alert--text">You must check the task before deleting it!</p>
-          <button class="btn alert--btn">OK</button>
-        </div>
-      `;
+      handler(alertBtn, 'click', () => template.remove());
+      template.appendChild(alertText);
+      template.appendChild(alertBtn);
 
-      const closeAlert = selector('.alert--btn');
-      handler(closeAlert, 'click', () => alert.remove());
-      return
-    }else{
-      todo.classList.add('fall');
-      todoItem.remove()
+      (document.body || parentElement).appendChild(template);
     }
 
-    deleteLocalStorage(todo);
-
-    handler(todo, 'transitionend', () => todo.remove())
+    return
   }
+  todo.classList.add('fall');
+  todoItem.remove();
+
+  deleteLocalStorage(todo);
+
+  handler(todo, 'transitionend', () => todo.remove());
 }
